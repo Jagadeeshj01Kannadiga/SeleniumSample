38b7eb6b0b01e6cb510da3eeff13503f2f622ec2
la-vn0nksm <la-vn0nksm>
2018-08-24 17:26:26 -0700

    First selenium

---
+++ src/test/java/Sample_Selenium_Project/Test1/TestOne.java
@@ -0,0 +1,36 @@
+package Sample_Selenium_Project.Test1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class TestOne {
+	
+	 static WebDriver driver;
+
+	
+	@BeforeTest
+	public void beforeTest() {
+		
+		System.setProperty("webdriver.chrome.driver", "C:/Jagadeesh/drivers/chromedriver.exe");
+		driver = new ChromeDriver();
+	}
+
+	
+	@Test
+	public void testOne() {
+		
+		driver.get("https://www.amazon.com/");
+	}
+	
+	@AfterTest
+	public void tearDown() {
+		driver.quit();
+		
+	}
+	
+	
+
+}
